c75a50194342cda6e41b635aaac9bfa1acf73947
Ahmed Dobaa <engahmeddobaa@gmail.com>
2021-04-28 13:09:43 +0200

    v1

--- test/products.test.js
+++ test/products.test.js
@@ -2,7 +2,7 @@ var chai= require('chai');
 
 const request= require('supertest');
 const app= require('../server');
-var accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIwLCJ1c2VyTmFtZSI6ImFobWVkIiwibW9iaWxlTnVtYmVyIjoiMDEwMTM4NDg4MDkiLCJ1c2VyQmFsYW5jZSI6MTAwMCwiaWF0IjoxNjE5NTcyMDczLCJleHAiOjE2MTk2NTg0NzN9.q4ASUXRDC6x9P-f4XUb_AxydL_0P7mYJL-horYgH18s';
+var accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQyLCJ1c2VyTmFtZSI6ImFobWVkIiwibW9iaWxlTnVtYmVyIjoiMDEwMTM4NDg4MDkiLCJpYXQiOjE2MTk2MDgxMDgsImV4cCI6MTYyODI0ODEwOH0.gX4oeDXru_FvcHg_YArqHyxQQ4vVu0IifJorcrqjh-Y';
 
 describe('Get /products', () =>{
     it('return list', () =>{
